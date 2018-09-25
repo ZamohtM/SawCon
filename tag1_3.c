0b835e4cf37788126daadd5ee406b8400507c022
Thomas <43405974+ZamohtM@users.noreply.github.com>
2018-09-25 18:40:31 +0200

    Update tag1_3.c

--- tag1_3.c
+++ tag1_3.c
@@ -5,7 +5,8 @@ int main()
     int counter=1;
     
     while(counter <= 100)
-    printf("Counter: %d\n", counter++);
+        printf("Counter: %d\n", counter);
+        counter++;
     
     printf("Takeoff");
     return 0;
