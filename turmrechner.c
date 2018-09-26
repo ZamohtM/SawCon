ab819a305264beb408b6e260221b6b8ef45829c1
Thomas <43405974+ZamohtM@users.noreply.github.com>
2018-09-26 08:28:47 +0200

    Create turmrechner.c

---
+++ turmrechner.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int start=0;
+    int result=0;
+    
+    printf("Geben Sie einen Startwert ein: ");
+    scanf("%d", &start);
+    result=start;
+    printf("Wire rechnen einen Turm mit dem Startwert %d\n",start);
+    
+    for(unsigned int i=1;i<=10;i++) {
+        int tempResult = result*i;
+        printf("%d * %d = %d\n", result,i,tempResult);
+        result = tempResult;
+    }
+    
+     for(unsigned int i=1;i<=10;i++) {
+        int tempResult = result/i;
+        printf("%d / %d = %d\n", result,i,tempResult);
+        result = tempResult;
+    }
+    
+    if(result==start) {
+        printf("Juhu es passt\n");
+    } else {
+        printf("Es passt nicht\n");
+    }
+}
