a6cd560acff6234995e014365e7196277bd95d52
Thomas <43405974+ZamohtM@users.noreply.github.com>
2018-11-07 09:06:27 +0100

    Update reeeee2.c

--- reeeee2.c
+++ reeeee2.c
@@ -3,24 +3,25 @@
 int counterSum(int numbers[], int length) {
 	int sum = 0;
 
-	for (int i = 0; i <= length; i++) {
+	for (int i = 0; i < length; i++) {
 		sum = sum + numbers[i];
 	}
 	printf("%d\n", sum);
 }
 
 int counterSumRec(int numbers[], int length) {
-    
+
     if (length) {
-        return numbers[length] + counterSumRec(numbers, length-1);
-    }return numbers[0];
+        return numbers[length-1] + counterSumRec(numbers, length-1);
+    }return 0;
 }
 
 int main() {
 
 	int numbers[] = {1,2,3,4,5};
-	int length = 4;
-
+	int length = 5;
+	
 	counterSum(numbers, length);
-    printf("%d\n", counterSumRec(numbers, length));
+   	 printf("%d\n", counterSumRec(numbers, length));
+
 }
