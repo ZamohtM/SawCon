b60acd95fafc2170be2e63320c7f43123c344d0b
Thomas <43405974+ZamohtM@users.noreply.github.com>
2018-11-14 09:17:50 +0100

    Update schachbrett.c

--- schachbrett.c
+++ schachbrett.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
+void ArrayFunc(int array[8][8]){
+    printf("Func 1 \n");
+    for(int i=0; i<8; i++){
+        for(int j=0;j<8;j++){
+            printf("%d ", array[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void ArrayFunc2(int *ptrArray) {
+    printf("Func 2 \n");
+    for(int i=0; i<8; i++){
+        for(int j=0;j<8;j++){
+            printf("%d ",*ptrArray++);
+        }
+        printf("\n");
+    }
+}
+
 int main(){
     
     int array[8][8];
@@ -8,7 +28,7 @@ int main(){
         for(int j=0;j<8;j++){
             array[i][j]=(i+1)*10+j+1;
         }
-    }
+    }    
     
     for(int i=0; i<8; i++){
         for(int j=0;j<8;j++){
@@ -29,5 +49,8 @@ int main(){
         printf("\n");
     }
     
+    ArrayFunc(array);
+    ArrayFunc2(array);
+
     return 0;
 }
