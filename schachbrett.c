4648518d65de8b38edbde2c4e6909cc668e85349
Thomas <43405974+ZamohtM@users.noreply.github.com>
2018-11-14 08:34:19 +0100

    Create schachbrett.c

---
+++ schachbrett.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    
+    int array[8][8];
+    
+    for(int i=0; i<8; i++){
+        for(int j=0;j<8;j++){
+            array[i][j]=(i+1)*10+j+1;
+        }
+    }
+    
+    for(int i=0; i<8; i++){
+        for(int j=0;j<8;j++){
+            printf("%d ", array[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
