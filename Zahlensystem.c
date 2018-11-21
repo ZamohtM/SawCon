1f90ef72141892b3e5ab50c9b3836b3033c4188f
Thomas <43405974+ZamohtM@users.noreply.github.com>
2018-11-21 09:21:05 +0100

    Update Zahlensystem.c

--- Zahlensystem.c
+++ Zahlensystem.c
@@ -1,30 +1,45 @@
 #include <stdio.h>
 #include <math.h>
 
-int functionOne(int base, int value[]) {
+int functionOne(int base, int arrayvalue[]) {
     
     int result = 0;
     int length = 2;
     
     for(int i=0; i<3; i++) {
-        result = result + value[i]*(pow(base, length));
+        result = result + arrayvalue[i]*(pow(base, length));
         length--;
     }
     printf("Ergebnis: %d\n", result);
 }
 
+int functionTwo(int base, int value) {
+    
+    int result = 0;
+    int rresult = 0;
+    
+    result = value/base;
+    rresult = value%base;
+    
+}
 
 int main() {
     
     int base = 0;
-    int value[3];
+    int arrayvalue[3];
+    int value = 0;
     
     printf("Geben Sie Ihre Basis ein!\n");
     scanf("%d", &base);
     printf("Geben Sie Ihren Startwert ein!\n");
     for(int i = 0; i<3;i++){
-        scanf("%d", value+i);
+        scanf("%d", arrayvalue+i);
     }
     
+    for(int j=0; j<3;j++){
+        value = 10 * value + arrayvalue[j];
+    }
+    
+    functionOne(base,arrayvalue);
     functionOne(base,value);
 }
