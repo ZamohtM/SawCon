6f666ffd239c5435c4e0a6f738f2ec930001f1e6
Thomas <43405974+ZamohtM@users.noreply.github.com>
2018-12-11 14:49:54 +0100

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -12,9 +12,9 @@ int main()
    erzeugeArray(z,anz);
    printf("Array mit %d Zahlen:\n", anz);
    zeigeArrayInKonsole(z, anz);
-   /*bubbleSort(z,anz);
+   bubbleSort(z,anz);
    printf("\nArray mit %d Zahlen sortiert:\n",anz);
-   zeigeArrayInKonsole(z,anz);*/
+   zeigeArrayInKonsole(z,anz);
    
    return 0;
 }
@@ -33,6 +33,21 @@ void zeigeArrayInKonsole(int * array, int len) {
    }
 }
 
-void bubbleSort(int * array, int len) {
-    
+void bubbleSort(int * array, int len)
+{
+   int i, j,tmp;
+
+   for (i = 1; i < len ; i++) 
+   {
+      for (j = 0; j < len - i ; j++) 
+      {
+          if (array[j] > array[j+1]) 
+          {
+              tmp = array[j];
+              array[j] = array[j+1];
+              array[j+1] = tmp;
+          }
+      }
+   }
 }
+
