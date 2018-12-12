4a46abacd8f369c04c9757f83ec46ee475e8f41e
Thomas <43405974+ZamohtM@users.noreply.github.com>
2018-12-12 09:29:05 +0100

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -6,6 +6,9 @@ int main()
     
    const int anz = 10;
    int z[anz];
+   int gaus = 0;
+   
+   gaus = (anz/2)*(1+anz);
    
    erzeugeArray(z,anz);
    printf("Array mit %d Zahlen:\n", anz);
@@ -13,6 +16,9 @@ int main()
    bubbleSort(z,anz);
    printf("\nArray mit %d Zahlen sortiert:\n",anz);
    zeigeArrayInKonsole(z,anz);
+   printf("\n\nMaximalzahl der nötigen Vergleiche wäre (%d-1)*(%d-1) = %d", anz, anz, (anz-1)*(anz-1));
+   printf("\nVerbesserte Version (pro Durchlauf 1 Vergleich weniger) wäre (9+8+7+6+ ... +2+1) = %d", gaus);
+   printf("\nVerbesserte Verbessete Version: %d", bubbleSort(z,anz));
    
    return 0;
 }
@@ -31,10 +37,11 @@ void zeigeArrayInKonsole(int * array, int len) {
    }
 }
 
-void bubbleSort(int * array, int len)
+int bubbleSort(int * array, int len)
 {
     int d, v, tmp;
     int getauscht = 0;
+    int anzVertauschungen = 0;
    for (d = 1; d < len ; d++) 
    {
        getauscht = 0;
@@ -47,9 +54,12 @@ void bubbleSort(int * array, int len)
               array[v+1] = tmp;
               getauscht = 1;
           }
+          anzVertauschungen++;
       }
       if (getauscht == 0) {
           break;
       }
+    
    }
+   return anzVertauschungen;
 }
