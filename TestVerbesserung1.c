4df9c991fe52ef03b4c5a6fd1f385a1063ceab64
Thomas <43405974+ZamohtM@users.noreply.github.com>
2019-01-23 08:31:10 +0100

    Update TestVerbesserung1.c

--- TestVerbesserung1.c
+++ TestVerbesserung1.c
@@ -2,83 +2,83 @@
 
 void fillArray(int *array, int size, int ug, int og)
 {
-    srand(time(0));
-    for(int i = 0; i < size; i++)
+    srand(time(0));
+    for(int i = 0; i < size; i++)
     {
-        array[i] = (rand()%(og-ug))+ug;
+        array[i] = (rand()%(og-ug))+ug;
     }
 }
 
 void printArray(int *array, int size)
 {
-    for(int i = 0; i < size; i++)
+    for(int i = 0; i < size; i++)
     {
         if(i%12==0) {
-            printf("\n");
+            printf("\n");
         }
-        printf("%d ", array[i]);
+        printf("%d ", array[i]);
     }
 }
 
 int min(int *array, int size)
 {
-    int min = array[0];
+    int min = array[0];
     
-    for(int i = 1; i < size; i++) 
+    for(int i = 1; i < size; i++) 
     {
         if(array[i] < min)
         {
-            min = array[i];
+            min = array[i];
         }
     }
-    return min;
+    return min;
 }
 
 int max(int *array, int size)
 {
-    int max = array[0];
+    int max = array[0];
     
-    for(int i = 1; i < size; i++) 
+    for(int i = 1; i < size; i++) 
     {
         if(array[i] > max)
         {
-            max = array[i];
+            max = array[i];
         }
     }
-    return max;
+    return max;
 }
 
 void buildNewArray(int *array, int size, int mini, int maxi)
 {
-    for(int i = 0; i < size; i++)
+    for(int i = 0; i < size; i++)
     {
         if(i%2==0) {
-            array[i] = mini;
+            array[i] = mini;
         }
         if(i%2!=0){
-            array[i] = maxi;
+            array[i] = maxi;
         }
     }
 }
 
 int main() {
     
-    int size = 20;
-    int array[size];
-    int ug = 300;
-    int og = 500;
-    int anzSpalten = 12;
+    int size = 20;
+    int array[size];
+    int ug = 300;
+    int og = 500;
+    int anzSpalten = 12;
     
-    fillArray(array, size, ug, og);
-    printf("Array Original: \n");
-    printArray(array, size);
+    fillArray(array, size, ug, og);
+    printf("Array Original: \n");
+    printArray(array, size);
     
-    int mini = min(array, size);
-    int maxi = max(array, size);
-    printf("\nArray Umgestellt: \n");
+    int mini = min(array, size);
+    int maxi = max(array, size);
+    printf("\nArray Umgestellt: \n");
     
-    buildNewArray(array, size, mini, maxi);
-    printArray(array, size);
+    buildNewArray(array, size, mini, maxi);
+    printArray(array, size);
     
-    return 0;
+    return 0;
 }
